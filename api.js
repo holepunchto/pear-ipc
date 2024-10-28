5e1ee43d6e8ec473ce831ffb4c7252680612a34d
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-10-28 15:09:06 +0100

    Remove waiting for lock on restart and let unload close the app

--- api.js
+++ api.js
@@ -25,26 +25,6 @@ class API extends Internal {
       this._pinging = false
 
       method.send(args)
-
-      if (!args.platform) return
-
-      const fd = await new Promise((resolve, reject) => fs.open(this.#ipc._lock, 'r+', (err, fd) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        resolve(fd)
-      }))
-      await fsext.waitForLock(fd)
-      fsext.unlock(fd)
-      await new Promise((resolve, reject) => fs.close(fd, (err) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        resolve()
-      }))
-      await this.#ipc.close()
     }
   }
 
