4b2f8ba62ca78ce01bbd28bf1ed648fd6f8b3415
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-10-28 14:00:33 +0100

    Switch restart to use send instead of request

--- api.js
+++ api.js
@@ -19,6 +19,35 @@ class API extends Internal {
     return (link, storage, appdev) => method.request({ args: [link, storage, appdev] })
   }
 
+  restart (method) {
+    return async (args) => {
+      if (this.#ipc.closed || this.#ipc.closing) return
+      this._pinging = false
+
+      method.send(args)
+
+      if (!args.platform) return
+
+      const fd = await new Promise((resolve, reject) => fs.open(this.#ipc._lock, 'r+', (err, fd) => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve(fd)
+      }))
+      await fsext.waitForLock(fd)
+      fsext.unlock(fd)
+      await new Promise((resolve, reject) => fs.close(fd, (err) => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve()
+      }))
+      await this.#ipc.close()
+    }
+  }
+
   shutdown (method) {
     return async () => {
       if (this.#ipc.closed || this.#ipc.closing) return
