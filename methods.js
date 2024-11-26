5732ced57f6175457ea0fa288033be3349a0ce01
dmc <david.mark.clements@gmail.com>
2024-11-26 20:09:05 +0100

    v2 methods

--- methods.js
+++ methods.js
@@ -36,7 +36,13 @@ const methods = [
   { id: 33, name: 'touch', stream: true },
   { id: 34, name: '_ping' },
   { id: 35, name: 'reset', stream: true },
-  { id: 36, name: 'data', stream: true }
+  { id: 36, name: 'data', stream: true },
+  { id: 37, name: 'get' },
+  { id: 38, name: 'entry' },
+  { id: 39, name: 'reported' }
+
+
+
 ]
 
 module.exports = methods
