8376106fbb7ad5fd597bb647d484b8cb05f9f710
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-03-13 09:17:47 +0100

    added shutdown lock poll

--- api.js
+++ api.js
@@ -2,6 +2,9 @@
 const fs = require('fs')
 const fsext = require('fs-native-extensions')
 
+const LOCK_POLL_INTERVAL = 500
+const POLL_MAX_TRIES = 20
+
 class Internal {
   _pinging = true
   _ping (method) { return () => this._pinging && method.request({ beat: 'ping' }) }
@@ -24,13 +27,21 @@ class API extends Internal {
       if (this.#ipc.closed || this.#ipc.closing) return
       this._pinging = false
       method.send()
-      const fd = await new Promise((resolve, reject) => fs.open(this.#ipc._lock, 'r+', (err, fd) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        resolve(fd)
-      }))
+      let tries = 0
+      const fd = await new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+          fs.open(this.#ipc._lock, 'r+', (err, fd) => {
+            if (!err) {
+              clearInterval(interval)
+              resolve(fd)
+            } else {
+              tries++
+              if (tries > POLL_MAX_TRIES) reject(err)
+            }
+          })
+        }, LOCK_POLL_INTERVAL)
+      })
+
       await fsext.waitForLock(fd)
       fsext.unlock(fd)
       await new Promise((resolve, reject) => fs.close(fd, (err) => {
