0ec501a63fa0c636d98327b8e38158ca9ee0af4c
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-03-21 10:42:52 +0100

    added startId to wakeup args

--- api.js
+++ api.js
@@ -19,7 +19,7 @@ class API extends Internal {
   }
 
   wakeup (method) {
-    return (link, storage, appdev, selfawake) => method.request({ args: [link, storage, appdev, selfawake] })
+    return (link, storage, appdev, selfawake, startId) => method.request({ args: [link, storage, appdev, selfawake, startId] })
   }
 
   shutdown (method) {
