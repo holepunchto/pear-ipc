ca921488fa117f2743371e667cf4425d5bc3fa5d
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2025-04-08 12:49:54 +0200

    added presets method (#37)

--- methods.js
+++ methods.js
@@ -42,7 +42,8 @@ const methods = [
   { id: 39, name: 'exists' },
   { id: 40, name: 'compare' },
   { id: 41, name: 'reported' },
-  { id: 42, name: 'dataReset', stream: true }
+  { id: 42, name: 'dataReset', stream: true },
+  { id: 43, name: 'presets', stream: true }
 ]
 
 module.exports = methods
