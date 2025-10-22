fd987266de9128307b937a9b2af1a131260a9b80
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-10-22 11:46:44 +0200

    move shutdown from rocksdb lock to platform lock

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 'use strict'
-const fs = require('fs')
-const fsext = require('fs-native-extensions')
+const LockFile = require('fs-native-lock')
 
 const LOCK_POLL_INTERVAL = 500
 const POLL_MAX_TRIES = 20
@@ -30,38 +29,9 @@ class API extends Internal {
       if (this.#ipc.closed || this.#ipc.closing) return
       this._pinging = false
       method.send()
-      const fd = await new Promise((resolve, reject) => {
-        let tries = 0
-        let interval = null
-        const poll = () => {
-          fs.open(this.#ipc._lock, 'r+', (err, fd) => {
-            if (!err) {
-              clearInterval(interval)
-              resolve(fd)
-            } else {
-              tries++
-              if (tries > POLL_MAX_TRIES) {
-                clearInterval(interval)
-                reject(err)
-              }
-            }
-          })
-        }
-        interval = setInterval(poll, LOCK_POLL_INTERVAL)
-        poll()
-      })
-
-      await fsext.waitForLock(fd)
-      fsext.unlock(fd)
-      await new Promise((resolve, reject) =>
-        fs.close(fd, (err) => {
-          if (err) {
-            reject(err)
-            return
-          }
-          resolve()
-        })
-      )
+      const platformLock = new LockFile(this.#ipc._lock, { wait: true })
+      await platformLock.lock()
+      await platformLock.unlock()
       await this.#ipc.close()
     }
   }
